3f6dfa720d5c79187856ca2c3c00dddece3ca2d2
Lucas Heinschke <heinschke123@gmail.com>
2024-12-13 09:29:55 +0100

    feat: update hero section layout to break skills

--- src/components/content/hero.tsx
+++ src/components/content/hero.tsx
@@ -102,7 +102,7 @@ export function HeroSection() {
                 <span className="text-amber-300">{personalInfo.aliasName}</span>
                 <span className="text-gray-400">{`',`}</span>
               </div>
-              <div className="ml-4 lg:ml-8 mr-2">
+              <div className="ml-4 lg:ml-8 mr-2 flex flex-wrap">
                 <span className=" text-white">skills:</span>
                 <span className="text-gray-400">{`[`}</span>
                 {personalInfo.skills.map((skill, index) => (
